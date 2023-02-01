9e362cd6f058e3ab800a2c66ffe8257a232ae717
Kevin-Zapata <kevin19981921@gmail.com>
2023-02-01 10:33:24 -0500

    Aprendiendo variables en JS

---
+++ JS-BASICO/variables.js
@@ -0,0 +1,9 @@
+var  nombre = "Kevin"; //Sintaxis para poder haceer una variable
+
+//declarar
+var edad; //Estamos declarando un variable
+
+//Iniciarlizar
+edad = 30 //Aqui estamos inicializando la variable, ya tiene un valor
+
+var elementos = ["computadora", "celular"]; //Estamos declarando e inicializando la variable<ap5
