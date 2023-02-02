a1015d73c5feb077a5febfbb1f700a763e2aa4f0
Kevin-Zapata <kevin19981921@gmail.com>
2023-02-02 16:36:12 -0500

    Aprendiendo sobre truthly y falsy

---
+++ JS-BASICO/Truthy y falsy.js
@@ -0,0 +1,11 @@
+Boolean(0) //El 0 Es falso "false"
+Boolean(null) // El null es falso
+Boolean(NaN) // El NaN es falso
+Boolean(undefined) // El undefined es falso
+Boolean("") //Un string vacio es falso
+Boolean("a") //Culquier letra es verdadero
+Boolean([]) // Un array vacio o lleno es verdadero
+Boolean({}) // Un objecto es verdadero
+Boolean(function(){}) // Una funcion es verdadero
+
+
