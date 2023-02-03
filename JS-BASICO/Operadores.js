049b5ef44859b860ec2e692fd3ceea328e8e6e7d
Kevin-Zapata <kevin19981921@gmail.com>
2023-02-02 20:44:21 -0500

    Aprendiendo operadores JS

---
+++ JS-BASICO/Operadores.js
@@ -0,0 +1,19 @@
+// Operadores binarios
+3 + 2 // =5 suma
+50 - 10 // =40 resta
+10 * 20 // =200 multiplicacion
+20/2 //=10 dividir
+
+"Kevin" + "Zapata" //Concatena los 2 strings y los junta convirtiendolos en un solo string quedaria = "Kevin Zapata"
+//Operadores Unitarios
+!false    // el ! es una negación, al colocarle un false, no daria un "true" porque si se coloca al lado del ! estariamos negando que es falso
+
+3 == "3" //Sirve para comparar, no compara el tipo de valor si no el valor como tal, aqui ambos son el valor 3
+3==="3" // En este verifica que el tipo de valor y el valor sean estrictamente iguales, osea en ambos el valor es 3 pero el tipo es diferente ya que uno es number y otro string
+5 < 3 //Menor que, valida que 5 sea menor que 3
+5 > 3 //Mayor que
+5 <= 3 // Menor e igual que
+a && b  // Ambas tienen que ser verdaderas, si no, no se cumplen
+a || b // Alguna tiene que ser verdad, se cumple la condicion
+var edad = 40
+edad++
